579c1f583fbd819c500f2a9dc58957149fe22859
Stirling McGee <stirlingmcgee@gmail.com>
2021-03-11 17:05:00 +0000

    Finished Hardware Controller Integration
    Moving from old hardware branch

--- controllers/paul_controller/paul_controller.py
+++ controllers/paul_controller/paul_controller.py
@@ -3,10 +3,16 @@
 
 # Import the API that should be used by the controller
 # Syntax: from <api_file_name> import paul
-from paul_skeleton import Paul
+from paul_webots import Paul
 
 # Import time library
 from time import time, sleep
+
+# Determine if we are currently working on hardware or sim
+hardware = False
+
+# Starting time of the robot (in seconds from epoch)
+start_time = time()
 
 # Instance of Paul class
 paul = Paul()
@@ -14,8 +20,9 @@ paul = Paul()
 # Time step (in seconds)
 # Use a sleep of at least 0.1, to avoid errors (This applies to Hardware implementations)
 time_step = 0.2
+
 # Max run time of the robot before termination (in seconds)
-run_time = 10
+run_time = 20
 
 # Used to stop the robot before the time is finished
 running = True
@@ -26,14 +33,18 @@ display = False
 # stores current direction & state
 going_up = True
 
-# Starting time of the robot (in seconds from epoch)
-start_time = time()
+# stores uv disinfection state
+uv_on = True
 
-# Start the robot moving up the pole
+# Start the initial movement of the robot
 paul.start_motors(paul.get_up_speed())
+paul.turn_uv_on()
 
-# Keep running the robot until it should stop or exceeds the run time
-while running and time() < start_time + run_time:
+# THIS WAS THE LINE PREVIOUS BUT FOR WHATEVER REASON IT CRASHES IF I USE IT
+# WEBOTS GETS ANGRY ABOUT RETURNING BUT UNCLEAR ON HOW TO FIX
+# while running and time() < start_time + run_time:
+
+while paul.robot.step(paul.timestep) != -1:
     try:
         # If the display option is enabled display all readings as defined in the API
         if display:
@@ -44,7 +55,7 @@ while running and time() < start_time + run_time:
             # Check that the robot has gone past the distance sensor stopping threshold
             if paul.check_top_ds_reading():
                 # If the robot has reached the ceiling the move the robot down
-                goingUp = False
+                going_up = False
                 paul.stop_motors()
                 paul.start_motors(paul.get_down_speed())
 
@@ -52,26 +63,33 @@ while running and time() < start_time + run_time:
         elif not going_up:
             if paul.check_bottom_ds_reading():
                 # If the robot has reached the floor then move the robot back up
-                goingUp = True
+                going_up = True
                 paul.stop_motors()
                 paul.start_motors(paul.get_up_speed())
         else:
-            # If there the robot is not moving up or down then there is an issue and the code should be terminated
+            # If there the robot is not moving up or down then there is an issue and the code should be
+            # terminated
             paul.stop_motors()
-            running = False
+            paul.turn_uv_off()
 
-        # Pause the program for a set duration
-        sleep(time_step)
+        # Pause the program for a set duration, not required on the simulation version
+        if hardware:
+            sleep(time_step)
 
-    # Catch any exceptions that occure during execution
+    # Catch any exceptions that occur during execution
     # these may affect the motors and lead to an unexpected termination of the loop
-    # A I/O error can occur during while using the Raspberry Pi API (This is unavoidable and should be handled)
+    # A I/O error can occur during while using the Raspberry Pi API (This is unavoidable and should be handled.
+
     except Exception as e:
+        pass
         # Note: Below print statement will only work in Python2
         # print str(e)
         # Stop and restart the motors if an error occurs
         paul.stop_motors()
-        paul.start_motors(paul.get_speed())
+        paul.turn_uv_off()
+        print("Error")
+        paul.display_all_readings()
 
-# Tidy up - code to be executed before the script terminates
-paul.stop_motors()
+        # I've just put 5 here for now, getting the speed won't work because it has just been stopped
+        # TODO: Figure out we want to do here
+        paul.start_motors(5)
---
+++ controllers/paul_controller/paul_webots.py
@@ -0,0 +1,147 @@
+# API for Webots used to define Paul class
+# Python 3
+
+# Imports go here
+from controller import Robot, Motor, DistanceSensor
+
+
+# Paul class is used to define a set of standard function calls (an API) to define how the controller interacts
+# with the environment (Webots, Hardware etc.)
+class Paul(object):
+    def __init__(self):
+        # Name of the controller
+        self.name = "PAUL controller for Webots"
+
+        # Current Speed of the robot (Used for exception handling recovery)
+        self.speed = 0
+        self.up_speed = 5
+        self.down_speed = -5
+
+        # May want to consider making both of these final variables
+        self.top_threshold = 1000
+        self.bottom_threshold = 1000
+
+        # create the Robot instance.
+        self.robot = Robot()
+
+        # get the time step of the current world.
+        self.timestep = int(self.robot.getBasicTimeStep())
+
+        # initialise the 3 wheels
+        self.wheels = []
+        wheelsNames = ['wheel1', 'wheel2', 'wheel3']
+
+        for i in range(3):
+            self.wheels.append(self.robot.getDevice(wheelsNames[i]))
+            self.wheels[i].setPosition(float('inf'))
+            self.wheels[i].setVelocity(0.0)  # unit: rad/s
+
+        # initialise distance sensors
+        self.top_ds = self.robot.getDevice('ds_top')
+        self.bottom_ds = self.robot.getDevice('ds_bottom')
+        self.top_ds.enable(self.timestep)
+        self.bottom_ds.enable(self.timestep)
+        # initialise LED
+        self.uv_led = self.robot.getDevice('uv_led')
+
+    # Movement Functions
+    # Function to start the robot moving at a speed defined in the speed parameter
+    def start_motors(self, speed):
+        print("Starting motors...")
+        self.speed = speed
+        for i in range(3):
+            self.wheels[i].setVelocity(speed)
+
+    # Function to stop the robot moving
+    def stop_motors(self):
+        print("Stopping motors...")
+        self.speed = 0
+        for i in range(3):
+            self.wheels[i].setVelocity(0)
+
+    # Motors and Sensors Readings
+    # Function to display all readings from the motors and sensors
+    def display_all_readings(self):
+        self.display_motor_readings()
+        self.display_top_ds_reading()
+        self.display_bottom_ds_reading()
+
+    # Function to display any readings from the motors
+    def display_motor_readings(self):
+        print("Motor speed: " + str(self.speed))
+
+    # Function get the current speed of the robot
+    # TODO: Potentially remove, equivalent to getSpeed?
+    def get_motor_readings(self):
+        return self.speed
+
+    # Function to check if the top distance sensor reading is within the threshold to trigger a change in direction
+    # Returns True  if the distance should change
+    # Returns False if the distance is not within the threshold
+    def check_top_ds_reading(self):
+        return self.get_top_ds_reading() < self.get_top_threshold()
+
+    # Function to display any readings from the top distance sensor
+    def display_top_ds_reading(self):
+        print("Top DS Reading: " + str(self.get_top_ds_reading()))
+
+    # Function to display get readings from the top distance sensor
+    def get_top_ds_reading(self):
+        return self.top_ds.getValue()
+
+    # Function to check if the top distance sensor reading is within the threshold to trigger a change in direction
+    # Returns True if the distance should change
+    # Returns False if the distance is not within the threshold
+    def check_bottom_ds_reading(self):
+        return self.get_bottom_ds_reading() < self.get_bottom_threshold()
+
+    # Function to display any readings from the bottom distance sensor
+    def display_bottom_ds_reading(self):
+        print("Bottom DS Reading: " + str(self.get_bottom_ds_reading()))
+
+    # Function to display get readings from the bottom distance sensor
+    def get_bottom_ds_reading(self):
+        return self.bottom_ds.getValue()
+
+    def turn_uv_on(self):
+        self.uv_led.set(255)
+
+    def turn_uv_off(self):
+        self.uv_led.set(0)
+
+    # Getters and Setters
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_speed(self):
+        return self.speed
+
+    def set_speed(self, speed):
+        self.speed = speed
+
+    def get_up_speed(self):
+        return self.up_speed
+
+    def set_up_speed(self, up_speed):
+        self.up_speed = up_speed
+
+    def get_down_speed(self):
+        return self.down_speed
+
+    def set_down_speed(self, down_speed):
+        self.down_speed = down_speed
+
+    def get_top_threshold(self):
+        return self.top_threshold
+
+    def set_top_threshold(self, top_threshold):
+        self.top_threshold = top_threshold
+
+    def get_bottom_threshold(self):
+        return self.bottom_threshold
+
+    def set_bottom_threshold(self, bottom_threshold):
+        self.bottom_threshold = bottom_threshold
